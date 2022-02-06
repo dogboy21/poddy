b52867fd26c70938c2efb14c99119601dbe92a7b
Dogboy21 <dogboy2103@gmail.com>
2022-02-06 16:54:19 +0100

    feat: add more UI feedback for workspace creation and deletion

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,7 +8,10 @@ export default {
             sessions: [],
             providers: [],
             workspaces: {},
+            workspacesList: [],
+
             repositoryInfo: null,
+            workspaceCreationError: null,
         }
     },
     methods: {
@@ -19,6 +22,12 @@ export default {
             location.replace('/oauth/logout/' + provider)
         },
         getRepositoryInfo() {
+            if (localStorage.getItem('pendingWorkspaceCreation')) {
+                let repoInfo = JSON.parse(localStorage.getItem('pendingWorkspaceCreation'))
+                localStorage.removeItem('pendingWorkspaceCreation')
+                return repoInfo
+            }
+
             let hash = location.hash
             if (!hash) {
                 return null
@@ -52,27 +61,49 @@ export default {
             }
         },
         startWorkspaceCreation() {
+            this.tabValue = 2
             axios.post('/api/v1/workspaces', this.repositoryInfo)
                 .then(creationResp => {
-                    location.replace(location.href.substr(0, window.location.href.indexOf('#')))
+                    history.replaceState(null, null, ' ')
+                    axios.get('/api/v1/workspaces')
+                        .then(resp => {
+                            this.repositoryInfo = null
+                            this.workspaces = resp.data
+                        })
+                        .catch(err => {
+                            console.error(err)
+                        })
                 })
                 .catch(err => {
                     console.log(err)
-                    $vaToast.error({ message: 'Failed to create workspace', closeable: false })
+                    this.workspaceCreationError = err
                 })
         },
         deleteWorkspace(workspace) {
+            let vaToast = this.$vaToast
             axios.delete('/api/v1/workspaces/' + workspace.provider + '/' + workspace.name)
                 .then(resp => {
-                    $vaToast.init({ message: 'Workspace ' + workspace.name + ' deleted', closeable: false })
+                    for (let provider of Object.keys(this.workspaces)) {
+                        this.workspaces[provider] = this.workspaces[provider].filter(filterWorkspace => filterWorkspace.name !== workspace.name)
+                    }
                 })
                 .catch(err => {
                     console.error(err)
-                    $vaToast.init({ message: 'Failed to delete workspace', closeable: false })
+                    vaToast.init({ message: 'Failed to delete workspace. Please try again later', closeable: false, color: 'danger' })
                 })
+        },
+        doLoginRedirect(repoInfo) {
+            let repositoryProvider = this.providers.filter(provider => provider.host === repoInfo.host)
+            if (!repositoryProvider) return
+
+            localStorage.setItem('pendingWorkspaceCreation', JSON.stringify(repoInfo))
+            location.replace('/oauth/auth/' + repositoryProvider[0].id)
         }
     },
     mounted() {
+        let vaToast = this.$vaToast
+        let creationRepo = this.getRepositoryInfo()
+
         axios.get('/oauth/providers')
             .then(providersResp => {
                 this.providers = providersResp.data
@@ -80,22 +111,40 @@ export default {
             })
             .then(selfResp => {
                 this.sessions = selfResp.data
-                if (selfResp.data.length === 0) return null
+                if (selfResp.data.length === 0) {
+                    if (creationRepo) this.doLoginRedirect(creationRepo)
+                    return null
+                }
                 return axios.get('/api/v1/workspaces')
             })
             .then(workspacesResp => {
                 if (!workspacesResp) return;
 
                 this.workspaces = workspacesResp.data
-                this.repositoryInfo = this.getRepositoryInfo()
+                this.repositoryInfo = creationRepo
                 if (this.repositoryInfo) {
                     this.startWorkspaceCreation()
                 }
             })
             .catch(err => {
                 console.error(err)
-                $vaToast.init({ message: 'Failed to load data', closeable: false })
+                if (creationRepo && this.providers && err.response && err.response.status === 401) this.doLoginRedirect(creationRepo)
+                vaToast.init({ message: 'Failed to load data', closeable: false, color: 'danger' })
             })
+    },
+    watch: {
+        workspaces: {
+            handler(val) {
+                this.workspacesList = Object.entries(val)
+                    .flatMap(entry => entry[1].map(workspace => {
+                        return {
+                            ...workspace,
+                            provider: entry[0],
+                        }
+                    }))
+            },
+            deep: true,
+        }
     },
     computed: {
         filteredProviders() {
@@ -105,18 +154,6 @@ export default {
                 }).length === 0
             })
         },
-        hasWorkspaces() {
-            return Object.keys(this.workspaces).length !== 0
-        },
-        workspacesList() {
-            return Object.entries(this.workspaces)
-                .flatMap(entry => entry[1].map(workspace => {
-                    return {
-                        ...workspace,
-                        provider: entry[0],
-                    }
-                }))
-        }
     }
 }
 </script>
@@ -128,7 +165,7 @@ export default {
             <va-card>
                 <va-card-title>Poddy</va-card-title>
 
-                <va-card-content v-if="repositoryInfo === null">
+                <va-card-content>
                     <va-tabs v-model="tabValue">
                         <template #tabs>
                             <va-tab>Sessions</va-tab>
@@ -175,8 +212,25 @@ export default {
                         </template>
 
                         <template #default v-else-if="tabValue === 2">
-                            <va-list v-if="hasWorkspaces">
+
+                            <va-alert color="danger" class="mb-4" v-if="workspaceCreationError">
+                                Failed to create workspace. Please try again later
+                            </va-alert>
+
+                            <va-list v-if="workspacesList.length > 0 || repositoryInfo">
                                 <va-list-label>Active Workspaces</va-list-label>
+
+                                <va-list-item v-if="repositoryInfo">
+                                    <va-list-item-section avatar>
+                                        <va-avatar color="danger" v-if="workspaceCreationError">{{ repositoryInfo.project.substring(0, 1).toUpperCase() }}</va-avatar>
+                                        <va-progress-circle indeterminate v-else />
+                                    </va-list-item-section>
+
+                                    <va-list-item-section>
+                                        <va-list-item-label>Creating workspace: {{ repositoryInfo.project }}</va-list-item-label>
+                                        <va-list-item-label caption><a :href="repositoryInfo.url" target="_blank" style="color:inherit;">{{ repositoryInfo.host }}</a></va-list-item-label>
+                                    </va-list-item-section>
+                                </va-list-item>
 
                                 <va-list-item v-for="workspace in workspacesList" v-bind:key="workspace.name">
                                     <va-list-item-section avatar>
@@ -197,10 +251,6 @@ export default {
                             <p v-else>You don't have any active workspaces currently</p>
                         </template>
                     </va-tabs>
-                </va-card-content>
-
-                <va-card-content v-else>
-                    <p>Please wait while your workspace for {{ repositoryInfo.project }} is being created...</p>
                 </va-card-content>
             </va-card>
         </div>
